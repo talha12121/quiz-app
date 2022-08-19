6a87692b2a80174b5067e70b1ef2e35949a6c1ed
Talha Khan <tk411635@gmail.com>
2022-08-19 17:46:47 +0500

    card ui completed

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
+var val=document.getElementById("login-btn")
+var email=document.getElementById("email")
+var password=document.getElementById("password")
 function btn(){ 
-    var val=document.getElementById("login-btn")
-    var email=document.getElementById("email")
-    var password=document.getElementById("password")
      var setvalue = localStorage.setItem("email",email.value)
      var setpass= localStorage.setItem("password",password.value)
     
@@ -9,6 +9,6 @@ function btn(){
          swal("Please enter correctly")
         }
         else{
-         window.location = "login.html";
+         window.location = "card.html";
         }
 }
--- signup.js
+++ signup.js
@@ -1,8 +1,30 @@
-var val1=document.getElementById("signBtn").value
-var getemail=document.getElementById("getemail").value
-var getpassword=document.getElementById("getpassword").value
+var val1=document.getElementById("signBtn")
+var getemail=document.getElementById("getemail")
+var getpassword=document.getElementById("getpassword")
+var getfname = document.getElementById("getfname")
+var getlname = document.getElementById("getlname")
 function getbtn(){
-var setvalue1 = localStorage.setItem("getemail",getemail)
-var setpass1= localStorage.setItem("getpassword",getpassword)
+var setvalue1 = localStorage.setItem("getemail",getemail.value);
+var setpass1= localStorage.setItem("getpassword",getpassword.value);
+var getfname1 = localStorage.setItem("getfname",getfname.value);
 
+if(getfname.value === "" || getlname.value === ""){
+    swal("PLEASE FILL THE FORM CORRECTLY")
+   }
+   else if(getemail.value === "" || getpassword.value === ""){
+   swal("PLEASE FILL THE FORM CORRECTLY");
+   }
+   else{
+    window.location = "index.html";
+   }
+
+// if(getemail.value === "" || getpassword.value === ""){
+//     swal("Please enter correctly")
+//    }
+// if(getfname.value === "" || getlname.value === ""){
+//     swal("Please enter name")
+//    }
+//    else{
+//     window.location = "login.html";
+//    }
 }
